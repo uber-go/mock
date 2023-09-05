d6d1e04765787ebb32bfe44b334cc4c9d4f4d17d
Ryan Hang <rhang@uber.com>
2023-09-05 12:11:50 -0700

    Fix go generate ./... issues under root
    Running `go generate ./...` on current main fails for a couple of reasons
    
    (1) The go:generate command in /mockgen/internal/tests/aux_imports_embedded_interface/bugreport.go
    has an -aux_file argument that specifies a package `faux` that is not in the
    stdlib and therefore not discoverable in GOROOT. Fixing this with a fully
    qualified path causes another failure that is known and documented
    in mockgen/internal/tests/aux_imports_embedded_interface/README.md.
    
    Because this failure is known, I've opted to comment out the command as
    documentation for reproducing the error.
    
    (2) mockgen/internal/tests/internal_pkg/generate.go attempts to mock an
    interface package with reflect mode and runs into fundamental go
    restrictions of `go build` with internal package paths outside of the
    internal package scope allowed by go.
    
    I've removed the generate directive here since this isn't a supported feature
    as the implementation of reflect mode creates a tempoorary directory and
    invokes `go build` there.
    
    ref: github.com/uber-go/mock/blob/8cd1a4cf46a6b1cf232b6862f2f11e81fe77a4e4/mockgen/reflect.go#L135
    ref: github.com/golang/mock/issues/29
    
    Not a failure fix but I've also changed type aliases of `any` back to type
    aliases of `interface {}`. Looking at the parsing logic, github.com/uber-go/mock/blob/8cd1a4cf46a6b1cf232b6862f2f11e81fe77a4e4/mockgen/parse.go#L752
    `interface{}` -> `any` causes *ast.TypeSpec to no longer type check to an *ast.InterfaceType
    and ends up removing generate mock code meant to test against regressions.
    
    ref: github.com/uber-go/mock/pull/48

--- mockgen/internal/tests/empty_interface/input.go
+++ mockgen/internal/tests/empty_interface/input.go
@@ -2,4 +2,4 @@ package empty_interface
 
 //go:generate mockgen -package empty_interface -destination mock.go -source input.go
 
-type Empty any
+type Empty interface{} // migrating interface{} -> any does not resolve to an interface type.
--- mockgen/internal/tests/internal_pkg/generate.go
+++ mockgen/internal/tests/internal_pkg/generate.go
@@ -1,4 +1,3 @@
 package test
 
-//go:generate mockgen -destination subdir/internal/pkg/reflect_output/mock.go go.uber.org/mock/mockgen/internal/tests/internal_pkg/subdir/internal/pkg Intf
 //go:generate mockgen -source subdir/internal/pkg/input.go -destination subdir/internal/pkg/source_output/mock.go
