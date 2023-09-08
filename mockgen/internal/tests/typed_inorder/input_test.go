0135608640d4cf78628bf87ca2fc11d309c98e63
eolmedo <esteban.olmedo.ramirez@gmail.com>
2023-09-08 06:20:16 +0000

    go fmt test files

--- mockgen/internal/tests/typed_inorder/input_test.go
+++ mockgen/internal/tests/typed_inorder/input_test.go
@@ -1,26 +1,27 @@
 package typed_inorder
 
 import (
-        "testing"
-        "fmt"
-        "go.uber.org/mock/gomock"
+	"fmt"
+	"testing"
+
+	"go.uber.org/mock/gomock"
 )
 
-func TestInteract(t * testing.T) {
-        ctrl := gomock.NewController(t)
+func TestInteract(t *testing.T) {
+	ctrl := gomock.NewController(t)
 
-        mockAnimal := NewMockAnimal(ctrl)
-        gomock.InOrder(
-                mockAnimal.EXPECT().Feed("burguir").DoAndReturn(func(s string) error {
-                        if s != "chocolate" {
-                                return nil
-                        }
-                        return fmt.Errorf("Dogs can't eat chocolate!")
-                }),
-                mockAnimal.EXPECT().GetSound().Return("Woof!"),
-        )
-        _, err := Interact(mockAnimal, "burguir")
-        if err != nil {
-                t.Fatalf("sad")
-        }
+	mockAnimal := NewMockAnimal(ctrl)
+	gomock.InOrder(
+		mockAnimal.EXPECT().Feed("burguir").DoAndReturn(func(s string) error {
+			if s != "chocolate" {
+				return nil
+			}
+			return fmt.Errorf("Dogs can't eat chocolate!")
+		}),
+		mockAnimal.EXPECT().GetSound().Return("Woof!"),
+	)
+	_, err := Interact(mockAnimal, "burguir")
+	if err != nil {
+		t.Fatalf("sad")
+	}
 }
