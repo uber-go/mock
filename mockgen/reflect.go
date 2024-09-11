60372e3e888c1a43656b5b495fabf1ba95fc2d6e
Sasha Melentyev <sasha@m8.ru>
2024-09-12 00:00:45 +0300

    chore: gofumpt -w . (#202)
    Signed-off-by: Sasha Melentyev <sasha@m8.ru>

--- mockgen/reflect.go
+++ mockgen/reflect.go
@@ -142,13 +142,13 @@ func runInDir(program []byte, dir string) (*model.Package, error) {
 		}
 	}()
 	const progSource = "prog.go"
-	var progBinary = "prog.bin"
+	progBinary := "prog.bin"
 	if runtime.GOOS == "windows" {
 		// Windows won't execute a program unless it has a ".exe" suffix.
 		progBinary += ".exe"
 	}
 
-	if err := os.WriteFile(filepath.Join(tmpDir, progSource), program, 0600); err != nil {
+	if err := os.WriteFile(filepath.Join(tmpDir, progSource), program, 0o600); err != nil {
 		return nil, err
 	}
 
