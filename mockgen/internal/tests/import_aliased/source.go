21f068b9ab831c7a9cef60da5ce3843555d76acf
Mihai Toader <mihai@sigmacomputing.com>
2024-11-15 09:31:40 -0800

    Use existing names

--- mockgen/internal/tests/import_aliased/source.go
+++ mockgen/internal/tests/import_aliased/source.go
@@ -1,10 +1,10 @@
 package import_aliased
 
 import (
-	definition_alias "github.com/package/definition"
+	definition_alias "github.com/stretchr/testify"
 )
 
-//go:generate mockgen -package import_aliased -destination source_mock.go -source=source.go -imports definition_alias=github.com/package/definition
+//go:generate mockgen -package import_aliased -destination source_mock.go -source=source.go -imports definition_alias=github.com/stretchr/testify
 
 type S interface {
 	M(definition_alias.X)
